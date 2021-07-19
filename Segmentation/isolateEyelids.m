c69e3ecfe98ca7e05ecc0565bf80d62fd649bdac
bradishp <bradishp@tcd.ie>
2021-07-19 23:55:10 +0100

    Updated eyelid isolation

--- Segmentation/isolateEyelids.m
+++ Segmentation/isolateEyelids.m
@@ -2,126 +2,109 @@
 function imagewithnoise = isolateEyelids(eyeImage, circleIris, circlePupil, ...
     irisColumnLower, irisColumnUpper, irisRowLower, irisRowUpper)
 
-irisRow = double(circleIris(1));
-irisCol = double(circleIris(2));
-irisRadius = double(circleIris(3));
-pupilRow = double(circlePupil(1));
-pupilCol = double(circlePupil(2));
-pupilRadius = double(circlePupil(3));
+    irisRow = double(circleIris(1));
+    irisCol = double(circleIris(2));
+    irisRadius = double(circleIris(3));
+    pupilRow = double(circlePupil(1));
+    pupilCol = double(circlePupil(2));
+    pupilRadius = double(circlePupil(3));
 
-validIris = logical(eyeImage);
+    % Get boundary points of the top and bottom eyelids
+    topBoundaryPoints = isolateEyelid(eyeImage, irisColumnLower, irisCol, ...
+        irisColumnUpper, irisRadius, irisRowLower, pupilRow-pupilRadius, 0);
 
-% Split top of the iris in three and find top eyelid for each of them
-irisThird = round((irisRadius * 2)/3);
-topEyelidLeft = eyeImage(irisRowLower:(pupilRow-pupilRadius), ...
-    irisColumnLower:irisColumnLower+irisThird);
-linesLeft = findline(topEyelidLeft);
-if size(linesLeft,1) > 0
-    [xLeft, yLeft] = linecoords(linesLeft, size(topEyelidLeft));
-    xLeft = xLeft + irisColumnLower;
-else
-    xLeft = [];
-    yLeft = [];
-end
+    heightOfExaminedArea = irisRowUpper - (pupilRow+pupilRadius) + 1;
+    bottomBoundaryPoints = isolateEyelid(eyeImage, irisColumnLower, irisCol, ...
+        irisColumnUpper, irisRadius, pupilRow+pupilRadius, irisRowUpper, ...
+        heightOfExaminedArea);
 
-bottomEyelidCentre = eyeImage(irisRowLower:(pupilRow-pupilRadius), ...
-    irisColumnLower + irisThird + 1:irisColumnLower + (2*irisThird));
-linesCentre = findline(bottomEyelidCentre);
-if size(linesCentre,1) > 0
-    [xCentre, yCentre] = linecoords(linesCentre, size(bottomEyelidCentre));
-    xCentre = xCentre + irisColumnLower + irisThird + 1;
-else
-    xCentre = [];
-    yCentre = [];
-end
+    validIris = logical(eyeImage);
+    xs = irisColumnLower:irisColumnUpper;
+    for ind = 1:size(xs, 2)
+        x = xs(ind);
+        topBoundaryPoint = topBoundaryPoints(ind);
+        bottomBoundaryPoint = bottomBoundaryPoints(ind);
 
-bottomEyelidRight = eyeImage(irisRowLower:(pupilRow-pupilRadius), ...
-    irisColumnLower + (2*irisThird) + 1:irisColumnUpper);
-linesRight = findline(bottomEyelidRight);
-if size(linesRight,1) > 0
-    [xRight, yRight] = linecoords(linesRight, size(bottomEyelidRight));
-    xRight = xRight + irisColumnLower + (2*irisThird) + 1;
-else
-    xRight = [];
-    yRight = [];
-end
+        validIris(1:ceil(topBoundaryPoint), x) = false;
+        validIris(floor(bottomBoundaryPoint):end, x) = false;
+    end
 
-% Combine two sides of the bottom eyelid
-xs = [xLeft, xCentre, xRight];
-ys = [yLeft, yCentre, yRight];
-ys = ys + irisRowLower;
+    % Further processing to try to remove pixels which aren't part of the iris
+    imagewithnoise = double(eyeImage);
 
-for ind = 1:size(xs, 2)
-    y = ys(ind);
-    x = xs(ind);
-    validIris(1:y, x) = false;
-end
+    [columnsInImage, rowsInImage] = meshgrid(1:size(eyeImage,2), 1:size(eyeImage,1));
 
-% Split bottom of the iris in two and find bottom eyelid for each of them
-bottomEyelidLeft = eyeImage((pupilRow+pupilRadius):irisRowUpper, ...
-    irisColumnLower:irisCol);
-linesLeft = findline(bottomEyelidLeft);
-if size(linesLeft,1) > 0
-    [xLeft, yLeft] = linecoords(linesLeft, size(bottomEyelidLeft));
-    xLeft = xLeft + irisCol - irisRadius;
-else
-    xLeft = [];
-    yLeft = [];
-end
+    pupilRegion = (rowsInImage - pupilRow).^2 ./ pupilRadius^2 ...
+        + (columnsInImage - pupilCol).^2 ./ pupilRadius^2 <= 1;
 
-bottomEyelidRight = eyeImage((pupilRow+pupilRadius):irisRowUpper, ...
-    irisCol+1:irisColumnUpper);
-linesRight = findline(bottomEyelidRight);
-if size(linesRight,1) > 0
-    [xRight, yRight] = linecoords(linesRight, size(bottomEyelidRight));
-    xRight = xRight + irisCol + 1;
-else
-    xRight = [];
-    yRight = [];
-end
+    irisRegion = (rowsInImage - irisRow).^2 ./ irisRadius^2 ...
+        + (columnsInImage - irisCol).^2 ./ irisRadius^2 <= 1;
+    irisRegion(pupilRegion) = false;
 
-% Combine two sides of the bottom eyelid
-xs = [xLeft, xRight];
-ys = [yLeft, yRight];
-ys = ys + pupilRow + pupilRadius + 2;
+    validIris = validIris & irisRegion;
 
-for ind = 1:size(xs, 2)
-    y = ys(ind);
-    x = xs(ind);
-    validIris(y:end, x) = false;
-end
+    imagewithnoise(~validIris) = NaN;
+
+    avg = mean(imagewithnoise, 'all', 'omitnan');
+    stdeviation = std(imagewithnoise, 0, 'all', 'omitnan');
+
+    % Adjust these values for datasets
+    upperThreshold = 2 * stdeviation;
+    lowerThreshold = 2 * stdeviation;
+
+    noiseFlags = ~logical(validIris);
+    noiseFlags(pupilRegion) = false;
+    noiseFlags(validIris) = ...
+        imagewithnoise(validIris) > avg + upperThreshold |...
+        imagewithnoise(validIris) < avg - lowerThreshold;
+
+    uncorruptRegions = imfill(~noiseFlags, 'holes');
+
+    % Remove extreme outliers which are caused by reflections and
+    % missclassified sections of the pupil
+    uncorruptRegions = uncorruptRegions & imagewithnoise >= 40 & ...
+        imagewithnoise <= 250;
+
+    imagewithnoise(~uncorruptRegions) = NaN;
+return
 
 
-% Further processing to try to remove pixels which aren't part of the iris
-imagewithnoise = double(eyeImage);
 
-[columnsInImage, rowsInImage] = meshgrid(1:size(eyeImage,2), 1:size(eyeImage,1));
+% Helper function to isolate eyelids
+function boundaryPoints = isolateEyelid(eyeImage, irisColumnLower, ...
+        irisCol, irisColumnUpper, irisRadius, eyelidMin, eyelidMax, ...
+        defualtBoundary)
+    sampleX = [irisColumnLower, irisCol, irisColumnUpper];
+    sampleY = [defualtBoundary, defualtBoundary, defualtBoundary];
 
-pupilRegion = (rowsInImage - pupilRow).^2 ./ pupilRadius^2 ...
-    + (columnsInImage - pupilCol).^2 ./ pupilRadius^2 <= 1;
+    % Split iris in three and find eyelid for each of them
+    irisThird = round((irisRadius * 2)/3);
+    eyelidLeft = eyeImage(eyelidMin:eyelidMax, ...
+        irisColumnLower:irisColumnLower+irisThird);
+    linesLeft = findline(eyelidLeft);
+    if size(linesLeft, 1) > 0
+        [~, yLeft] = linecoords(linesLeft, size(eyelidLeft));
+        sampleY(1) = yLeft(1);
+    end
 
-irisRegion = (rowsInImage - irisRow).^2 ./ irisRadius^2 ...
-    + (columnsInImage - irisCol).^2 ./ irisRadius^2 <= 1;
-irisRegion(pupilRegion) = false;
+    eyelidCentre = eyeImage(eyelidMin:eyelidMax, irisColumnLower + ...
+        irisThird + 1:irisColumnLower + (2*irisThird));
+    linesCentre = findline(eyelidCentre);
+    if size(linesCentre, 1) > 0
+        [~, yCentre] = linecoords(linesCentre, size(eyelidCentre));
+        sampleY(2) = yCentre(round(size(yCentre, 2)/2));
+    end
 
-validIris = validIris & irisRegion;
+    eyelidRight = eyeImage(eyelidMin:eyelidMax, irisColumnLower + ...
+        (2*irisThird) + 1:irisColumnUpper);
+    linesRight = findline(eyelidRight);
+    if size(linesRight, 1) > 0
+        [~, yRight] = linecoords(linesRight, size(eyelidRight));
+        sampleY(3) = yRight(end);
+    end
 
-imagewithnoise(~validIris) = NaN;
-
-avg = mean(imagewithnoise, 'all', 'omitnan');
-stdeviation = std(imagewithnoise, 0, 'all', 'omitnan');
-
-% Adjust these values for datasets
-upperThreshold = 2 * stdeviation;
-lowerThreshold = 2 * stdeviation;
-
-noiseFlags = ~logical(validIris);
-noiseFlags(pupilRegion) = false;
-noiseFlags(validIris) = ...
-    imagewithnoise(validIris) > avg + upperThreshold |...
-    imagewithnoise(validIris) < avg - lowerThreshold;
-
-uncorruptRegions = imfill(~noiseFlags, 'holes');
-imagewithnoise(~uncorruptRegions) = NaN;
-
+    % Combine three sides of the eyelid
+    irisColumns = irisColumnLower:irisColumnUpper;
+    boundaryPoints = interp1(double(sampleX), double(sampleY), irisColumns);
+    boundaryPoints = boundaryPoints + eyelidMin;
 return
